5c32ad0b2cc03d4defa8f860c62dbc9698fd11a2
xixiibn5100 <1584914306@qq.com>
2024-02-02 11:34:55 +0800

    fix:减小了卡片的宽度,修复了顶部图片的缩放问题

--- src/pages/suit/information/index.vue
+++ src/pages/suit/information/index.vue
@@ -6,7 +6,7 @@
         <image src="@/assets/photos/suitapply-suitInformation.svg"></image>
       </view>
       <view :class="style.flexColumn" style="width: 80% ;margin: 0 auto;">
-        <Card title="| 我的信息" style="color: var(--wjh-color-primary) ">
+        <Card title="| 我的信息" style="color: var(--wjh-color-primary)">
           <view :class="style.text1"> 学号 </view>
           <view :class="style.text4">{{ nowData.student_id }}</view>
           <view
